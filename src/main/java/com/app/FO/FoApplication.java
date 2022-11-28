39f1c019c5ec7f01604ba72b05a6f3e3a4ed9a8b
Cristi_ROG <cristian.givan.it@gmail.com>
2022-11-28 07:00:53 +0200

    first commit

---
+++ src/main/java/com/app/FO/FoApplication.java
@@ -0,0 +1,13 @@
+package com.app.FO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/FO/FoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.FO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
